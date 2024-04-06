0db4ce61173783f3c3f6ea3749ebcdf9d5efc679
Alisa13003 <akasionova2@gmail.com>
2024-04-06 14:39:58 -0400

    edited app.js

---
+++ Portfolio/app.js
@@ -0,0 +1,17 @@
+const sections = document.querySelectorAll(".section");
+const secBtns = document.querySelectorAll(".controls");
+const secBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector("main-content");
+
+function PageTransitions() {
+  for (let i = 0; i < secBtn.length; i++) {
+    /* loops through all the buttons */
+    secBtn[i].addEventListener("click", function () {
+      let currentBtn = document.querySelector(".active-btn");
+      currentBtn.className = currentBtn.className.replace("active-btn", "");
+      this.className += " active-btn";
+    });
+  }
+}
+
+PageTransitions();
