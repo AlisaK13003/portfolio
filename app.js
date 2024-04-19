bf35f680a011acd3e936a46a09bbaeee3b8239d0
Alisa13003 <akatsionova2@gmail.com>
2024-04-19 13:10:10 -0400

    finished header, began about

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ function PageTransitions() {
   for (let i = 0; i < secBtn.length; i++) {
     /* loops through all the buttons */
     secBtn[i].addEventListener("click", function () {
-      let currentBtn = document.querySelector(".active-btn");
-      currentBtn.className = currentBtn.className.replace("active-btn", "");
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].className = currentBtn[0].className.replace("active-btn", "");
       this.className += " active-btn";
     });
   }
