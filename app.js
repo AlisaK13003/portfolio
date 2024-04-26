41f999f48a1bb415261e41238167ae06acc6e6a4
AlisaK13003 <akatsionova2@gmail.com>
2024-04-26 18:36:25 -0400

    finished about and portfolio page

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
-const sections = document.querySelectorAll(".section");
-const secBtns = document.querySelectorAll(".controls");
-const secBtn = document.querySelectorAll(".control");
-const allSections = document.querySelector(".main-content");
-
-function PageTransitions() {
-  for (let i = 0; i < secBtn.length; i++) {
-    /* loops through all the buttons */
-    secBtn[i].addEventListener("click", function () {
-      let currentBtn = document.querySelectorAll(".active-btn");
-      currentBtn[0].className = currentBtn[0].className.replace("active-btn", "");
-      this.className += " active-btn";
-    });
-  }
-
-  allSections.addEventListener("click", (e) =>{
-    const id = e.target.dataset.id;
-    if (id){
-      secBtns.forEach((btn) =>{
-        btn.classList.remove('active')
-
+(function () {
+  [...document.querySelectorAll(".control")].forEach(button => {
+      button.addEventListener("click", function() {
+          document.querySelector(".active-btn").classList.remove("active-btn");
+          this.classList.add("active-btn");
+          document.querySelector(".active").classList.remove("active");
+          document.getElementById(button.dataset.id).classList.add("active");
       })
-      e.target.classList.add('active')
-
-      sections.forEach((section) =>{
-        section.classList.remove('active')
-      })
-
-      const element = document.getElementById(id);
-      element.classList.add('active');
-    }
-
+  });
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+      document.body.classList.toggle("light-mode");
   })
-}
-
-
-PageTransitions();
+})();
